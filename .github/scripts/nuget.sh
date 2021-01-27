62f42e5e0ece2ebd8b5b86d027da2534fe48c02b
Mark Gil Libres <markglibres@gmail.com>
2021-01-27 18:48:11 +1100

    fix nuget scripts

--- .github/scripts/nuget.sh
+++ .github/scripts/nuget.sh
@@ -4,17 +4,37 @@ set -e
 FILEPATH=$(dirname "$0")
 SCRIPTDIR=$(cd "$(dirname "$FILEPATH")"; pwd -P)/$(basename "$FILEPATH")
 
-cd $SCRIPTDIR
-cd ../../src/Server/IdentityServer
-dotnet pack IdentityServer.csproj -c Release --output nupkgs
-dotnet nuget push nupkgs/*.nupkg --api-key ${NUGET_APIKEY} --source https://api.nuget.org/v3/index.json
+NUPKGS_PATH=()
 
 cd $SCRIPTDIR
-cd ../../src/Hosts/IdentityServer.Hosts.Server
+cd ../../src/Server/IdentityServer
+NUPKGS_PATH=(${NUPKGS_PATH[@]} $(pwd))
+dotnet pack IdentityServer.csproj -c Release --output nupkgs
+git config --local user.email "markglibres@gmail.com"
+git config --local user.name "GitHub Action"
+git add IdentityServer.csproj
+git commit -m "bump version" -a
+
+cd $SCRIPTDIR
+cd ../../src/Server/Hosts/IdentityServer.Hosts.Server
+NUPKGS_PATH=(${NUPKGS_PATH[@]} $(pwd))
 dotnet pack IdentityServer.Hosts.Server.csproj -c Release --output nupkgs
-dotnet nuget push nupkgs/*.nupkg --api-key ${NUGET_APIKEY} --source https://api.nuget.org/v3/index.json
+git config --local user.email "markglibres@gmail.com"
+git config --local user.name "GitHub Action"
+git add IdentityServer.Hosts.Server.csproj
+git commit -m "bump version" -a
 
 cd $SCRIPTDIR
 cd ../../src/Server/IdentityServer.User.Client
+NUPKGS_PATH=(${NUPKGS_PATH[@]} $(pwd))
 dotnet pack IdentityServer.User.Client.csproj -c Release --output nupkgs
-dotnet nuget push nupkgs/*.nupkg --api-key ${NUGET_APIKEY} --source https://api.nuget.org/v3/index.json
+git config --local user.email "markglibres@gmail.com"
+git config --local user.name "GitHub Action"
+git add IdentityServer.User.Client.csproj
+git commit -m "bump version" -a
+
+echo "nuget packages ${NUPKGS_PATH}"
+for value in "${NUPKGS_PATH[@]}"
+do
+     dotnet nuget push $value/nupkgs/*.nupkg --api-key ${NUGET_APIKEY} --source https://api.nuget.org/v3/index.json
+done
